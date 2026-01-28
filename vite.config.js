f67341f3b3ff9ac52181faf22ed87dbdd957a6f5
Anant Kumar Jha <jhasonu136@gmail.com>
2026-01-29 02:42:16 +0530

    fix base path for netlify

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: "./",          // 🔥 ye line sabse important
     plugins: [react()],
     server: {
         port: 3001,
